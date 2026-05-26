026f8ae481c83025690a8802bce5156d5e662394
Ariyan Serhat Shane Graf <s_graf24@stud.hwr-berlin.de>
2026-05-26 10:04:26 +0200

    Reisestil und Reise als Klasse hinzugefügt mit allen notwendigen Eigenschaften

--- app/models.py
+++ app/models.py
@@ -22,5 +22,24 @@ class User(db.Model, UserMixin):
     def get_id(self):
         return str(self.u_id)
     
+class TravelStyle(db.Model):
+    __tablename__ = "travel_styles"
+    ts_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    trips = db.relationship('Trip', back_populates='travel_style')
 
-    
+
+class Trip(db.Model):
+    __tablename__ = "trips"
+    t_id = db.Column(db.Integer, primary_key=True)
+    host_u_id = db.Column(db.Integer, db.ForeignKey("users.u_id"), nullable=False)
+    travel_style_id = db.Column(db.Integer, db.ForeignKey("travel_styles.ts_id"), nullable=False)
+    location = db.Column(db.String(255), nullable=False)                            # Reiseziel = Startpunkt
+    continent = db.Column(db.String(50), nullable=False)                            # aus fester Python-Liste
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    max_participants = db.Column(db.Integer, nullable=False)
+    budget_min = db.Column(db.Integer, nullable=False)
+    budget_max = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
