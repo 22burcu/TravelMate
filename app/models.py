cdb4d2475cb607ffb673a8b5b65c7463dac35380
Ariyan Serhat Shane Graf <s_graf24@stud.hwr-berlin.de>
2026-05-26 10:16:53 +0200

    SQLAlchemy-Modelle für User, Trip, Application und TravelStyle eingepflegt (Version 1)

--- app/models.py
+++ app/models.py
@@ -43,3 +43,27 @@ class Trip(db.Model):
     budget_max = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+
+    host = db.relationship('User', back_populates='trips')
+    travel_style = db.relationship('TravelStyle', back_populates='trips')
+    applications = db.relationship('Application', back_populates='trip')
+
+class Application(db.Model):
+    __tablename__ = "applications"
+    a_id = db.Column(db.Integer, pimary_key=True)
+    trip_t_id = db.Column(db.Integer, db.ForeignKey("trips.t_id"), nullable=False)
+    joiner_u_id = db.Column(db.Integer, db.ForeignKey("users.u_id"), nullable=False)
+    message = db.Column(db.Text)
+    budget_min = db.Column(db.Integer, nullable=False)
+    budget_max = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pending')
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+
+    trip = db.relationship('Trip', back_populates ='applications')
+    joiner = db.relationship('User', back_populates = 'applications')
+
+    __table_args__ = (
+        db.UniqueConstraint('trip_t_id', 'joiner_u_id', name='unique_application'),  # Ein Joiner kann sich pro Trip nur einmal bewerben
+    )
+
+
