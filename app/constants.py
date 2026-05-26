7123c5d9ecca11fa7767642a09f3502658cb5e0b
Ariyan Serhat Shane Graf <s_graf24@stud.hwr-berlin.de>
2026-05-26 13:10:18 +0200

    Konstanten für Kontinen hinzugefügt

---
+++ app/constants.py
@@ -0,0 +1,12 @@
+# Festgelegte Auswahlmöglichkeiten, welche NICHT in der Datenbank gespeichert werden
+
+
+CONTINENTS = [
+    "Europa",
+    "Asien",
+    "Afrika",
+    "Nordamerika",
+    "Südamerika",
+    "Ozeanien",
+    "Antarktis",
+]
