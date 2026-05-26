fc32efd0bbe98b8d79168cfa2e73414793878e4c
Ariyan Serhat Shane Graf <s_graf24@stud.hwr-berlin.de>
2026-05-26 13:14:31 +0200

    App-Factory mit DB-Anbindung, Flask-Login und main-Blueprint eingerichtet

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from .models import db, User
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+
+
+def create_app():
+    app = Flask(__name__)
+
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travelmate.db"
+    app.config["SECRET_KEY"] = "travelmate-secret-key"
+
+
+    db.init_app(app)
+
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    from . import main
+    app.register_blueprint(main.bp)
+
+    return app
