da5d660f125e8048453bba15e855d7949c0fb26e
burcu <206716304+22burcu@users.noreply.github.com>
2026-06-22 12:26:31 +0200

    add datetime to dashboard.py again

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from .models import Trip, db, Application
+from datetime import date
 
 dashboard_bp = Blueprint("dashboard", __name__)
 @dashboard_bp.route("/dashboard")
