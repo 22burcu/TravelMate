ee7821865bb7adebefd7280b62002396d6685031
burcu <206716304+22burcu@users.noreply.github.com>
2026-07-16 19:37:45 +0200

    add trip

--- app/dashboard.py
+++ app/dashboard.py
@@ -3,13 +3,13 @@ from flask_login import login_required, current_user
 from .models import Trip, db, Application
 from datetime import date
 
-dashboard_bp = Blueprint("dashboard", __name__)
+dashboard_bp = Blueprint("dashboard", __name__) #Ein Blueprint ist wie ein Ordner für zusammengehörige Routen.
 @dashboard_bp.route("/dashboard")
-@login_required
+@login_required      #Nur eingeloggte Nutzer dürfen diese Seite aufrufen 
 
 def host_dashboard():
         today = date.today()
-        open_trips = Trip.query.filter(             #Quelle zu: https://docs.sqlalchemy.org/en/20/orm/queryguide/
+        open_trips = Trip.query.filter(            
             Trip.host_u_id == current_user.u_id,
             Trip.start_date > today
         ).all()  
@@ -35,3 +35,10 @@ def host_dashboard():
 def joiner_dashboard():
     applications = Application.query.filter_by(joiner_u_id=current_user.u_id).all()
     return render_template("joiner_dashboard.html", applications=applications)
+
+#-----------------------------------------------------------------------------
+# QUELLEN
+#-----------------------------------------------------------------------------
+# current_user              https://flask-login.readthedocs.io/en/latest/#flask_login.current_user
+# SQLAlchemy Queries        https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/queries/
+#                           https://docs.sqlalchemy.org/en/20/orm/queryguide/
