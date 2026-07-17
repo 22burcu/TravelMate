105a61d6d0f8a0e99f0c5215514107d06a5478d3
Oliwia Nogacz <s_nogacz22@stud.hwr-berlin.de>
2026-07-17 05:53:17 +0200

    add comments to init py

--- app/__init__.py
+++ app/__init__.py
@@ -2,31 +2,34 @@ from flask import Flask
 from .models import db, User
 from flask_login import LoginManager
 
-login_manager = LoginManager()
+#erstellt die flask anwendung, verbindet datenbanken und loginsysteme
+#und registriert blueprints damit flask weiß welche bereiche der webapp existieren
 
+
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travelmate.db"
-    app.config["SECRET_KEY"] = "travelmate-secret-key"
+    app.config["SECRET_KEY"] = "travelmate-secret-key"      #für flash messages, sessions, login sicherheit
 
 
-    db.init_app(app)
+    db.init_app(app)        #datenbank verbinden
 
 
-    login_manager.init_app(app)
+    login_manager.init_app(app)               #verbindet login system mit flask
     login_manager.login_view = "auth.login"
 
 
-    @login_manager.user_loader
+    @login_manager.user_loader          #eingeloggten user in db finden
     def load_user(user_id):
         return User.query.get(int(user_id))
     
     from . import main
     from .auth import auth_bp
-    from .trips import trips_bp
+    from .trips import trips_bp             # blueprint aus trips.py importiert
     from .dashboard import dashboard_bp
     from .profile import profile_bp 
     from .application import applications_bp
@@ -34,7 +37,7 @@ def create_app():
 
     app.register_blueprint(main.bp)
     app.register_blueprint(auth_bp)
-    app.register_blueprint(trips_bp)
+    app.register_blueprint(trips_bp)        #nach import registrieren damit flask sie erkennt und nutzen kann
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(profile_bp)
     app.register_blueprint(applications_bp)
