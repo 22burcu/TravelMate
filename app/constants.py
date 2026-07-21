d8017114d42b0827b0a729df365b5fbef8af1eaa
Oliwia Nogacz <s_nogacz22@stud.hwr-berlin.de>
2026-07-21 14:47:06 +0200

    add sources + comments

--- app/constants.py
+++ app/constants.py
@@ -10,3 +10,10 @@ CONTINENTS = [
     "Ozeanien",
     "Antarktis",
 ]
+
+'''quellen
+youtube python konzepte                 https://www.youtube.com/watch?v=Gx5qb1uHss4
+youtube python konzepte                 https://www.youtube.com/watch?v=0yrlE_XCMEE
+python liste                            https://docs.python.org/3/tutorial/datastructures.html#more-on-lists
+konstanten in python (großschreibung)   https://peps.python.org/pep-0008/#constants
+'''
